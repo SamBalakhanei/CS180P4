da73084d1311a5d3223779a16f21b93ab8e98a50
niha-raj <143101444+niha-raj@users.noreply.github.com>
2023-11-11 10:45:21 -0600

    Update TutorView.java

--- TutorView.java
+++ TutorView.java
@@ -1,25 +1,32 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class TutorView {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
         int choice = 0;
-        BufferedReader bfr = new BufferedReader(new FileReader("Customers.txt"));
+        BufferedReader bfr = new BufferedReader(new FileReader("accountDetails.txt"));
         String line = "";
+        ArrayList<String> foundPeople = new ArrayList<>();
         boolean found = false;
+        int finalNumber = 0;
+        String finalSelection = "";
         do {
-            String finalSelection = "";
             System.out.println("Select one of the options below:\n1. View list of students\n2. Search for a student");
             choice = scanner.nextInt();
             scanner.nextLine();
             int counter = 1;
             if (choice == 1) {
                 line = bfr.readLine();
-                while (line != null) {
-                    System.out.println(counter + ". " + line);
-                    counter++;
+                while(line != null) {
+                    String[] splitLine = line.split(":");
+                    if (Boolean.parseBoolean(splitLine[2])) {
+                        foundPeople.add(splitLine[0]);
+                        System.out.println(counter + ". " + foundPeople.get(counter - 1));
+                        counter++;
+                    }
                     line = bfr.readLine();
                 }
                 System.out.println("Select from students found:");
@@ -32,10 +39,14 @@ public class TutorView {
                     scanner.nextLine();
                     line = bfr.readLine();
                     while (line != null) {
-                        if (line.contains(name)) {
-                            System.out.println(counter + ". " + line);
-                            counter++;
-                            found = true;
+                        String[] splitLine = line.split(":");
+                        if (splitLine[0].toLowerCase().contains(name.toLowerCase())) {
+                            if (Boolean.parseBoolean(splitLine[2])) {
+                                foundPeople.add(splitLine[0]);
+                                System.out.println(counter + ". " + foundPeople.get(counter - 1));
+                                counter++;
+                                found = true;
+                            }
                         }
                         line = bfr.readLine();
                     }
@@ -44,8 +55,15 @@ public class TutorView {
                         searchAgain = scanner.nextLine();
                     }
                 } while (!found && searchAgain.equalsIgnoreCase("yes"));
-                System.out.println("Select from students found:");
-
+                if (found) {
+                    System.out.println("Select from tutors found:");
+                    finalNumber = scanner.nextInt();
+                    finalSelection = foundPeople.get(finalNumber - 1);
+                }
+                if (searchAgain.equalsIgnoreCase("no")) {
+                    System.out.println("Thank you for using TutorFinder! Goodbye.");
+                    break;
+                }
             } else {
                 System.out.println("Invalid option! Please try again!");
             }
