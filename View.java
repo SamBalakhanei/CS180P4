55b2a6089c6a0ef0cef9eeb299235e1c341e5be6
niha-raj <143101444+niha-raj@users.noreply.github.com>
2023-11-11 22:05:35 -0600

    Error handling on View.java
    This takes care of errors whenever there is an input from the user (if you spot a while (true) loop, that's what it is for)

--- View.java
+++ View.java
@@ -1,31 +1,39 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class View {
     //if user is a tutor
     public static User findStudent() {
         Scanner scanner = new Scanner(System.in);
-        String choice = "";
         String finalSelection = "";
         try {
             BufferedReader bfr = new BufferedReader(new FileReader("accountDetails.txt"));
             String line = "";
+            int choice = 0;
             ArrayList<String> foundPeople = new ArrayList<>();
             boolean found = false;
             int finalNumber = 0;
+            String invalidInput = "Invalid input. Please try again.";
             System.out.println("Select one of the options below:\n1. View list of students\n2. Search for a student");
-            choice = scanner.nextLine();
-            while (!choice.equals("1") && !choice.equals("2")) {
-                System.out.println("Invalid option! Please try again.");
-                System.out.println("Select one of the options below:\n1. View list of students\n2. Search for a student");
-                choice = scanner.nextLine();
-                scanner.nextLine();
+            while (true)  {
+                try {
+                    choice = scanner.nextInt();
+                    scanner.nextLine();
+                    if (choice == 1 || choice == 2)
+                        break;
+                    System.out.println(invalidInput);
+                } catch (InputMismatchException e) {
+                    scanner.next();
+                    System.out.println(invalidInput);
+                }
             }
             int counter = 1;
-            if (choice.equals("1")) {
+            if (choice == 1) {
                 line = bfr.readLine();
                 while (line != null) {
                     String[] splitLine = line.split(":");
@@ -37,8 +45,18 @@ public class View {
                     line = bfr.readLine();
                 }
                 System.out.println("Select from students found:");
-                finalNumber = scanner.nextInt();
-                scanner.nextLine();
+                while (true) {
+                    try {
+                        finalNumber = scanner.nextInt();
+                        scanner.nextLine();
+                        if (finalNumber < counter && finalNumber > 0)
+                            break;
+                        System.out.println(invalidInput);
+                    } catch (InputMismatchException e) {
+                        System.out.println(invalidInput);
+                        scanner.next();
+                    }
+                }
                 finalSelection = foundPeople.get(finalNumber - 1);
             } else {
                 String searchAgain = "";
@@ -62,21 +80,35 @@ public class View {
                     bfr.close();
                     if (!found) {
                         System.out.println("User not found! Would you like to search again? (Yes/No)");
-                        searchAgain = scanner.nextLine();
-                        if (searchAgain.equalsIgnoreCase("no")) {
-                            System.out.println("Thank you for using TutorFinder! Goodbye.");
-                            return null;
+                        while (true) {//!searchAgain.equalsIgnoreCase("no") && !searchAgain.equalsIgnoreCase("yes")) {
+                            searchAgain = scanner.nextLine();
+                            if (searchAgain.equalsIgnoreCase("no")) {
+                                System.out.println("Thank you for using TutorFinder! Goodbye.");
+                                return null;
+                            } else if (searchAgain.equalsIgnoreCase("yes")) {
+                                break;
+                            }
+                            System.out.println(invalidInput);
                         }
-                        bfr = new BufferedReader(new FileReader("accountDetails.txt"));
                     }
+                    bfr = new BufferedReader(new FileReader("accountDetails.txt"));
                 } while (!found && searchAgain.equalsIgnoreCase("yes"));
                 if (found) {
                     System.out.println("Select from students found:");
-                    finalNumber = scanner.nextInt();
-                    scanner.nextLine();
+                    while (true) {
+                        try {
+                            finalNumber = scanner.nextInt();
+                            scanner.nextLine();
+                            if (finalNumber < counter && finalNumber > 0)
+                                break;
+                            System.out.println(invalidInput);
+                        } catch (InputMismatchException e) {
+                            System.out.println(invalidInput);
+                            scanner.next();
+                        }
+                    }
                     finalSelection = foundPeople.get(finalNumber - 1);
                 }
-
             }
         } catch (IOException e) {
             // TODO Auto-generated catch block
@@ -88,23 +120,30 @@ public class View {
     //if user is a student
     public static User findTutor() {
         Scanner scanner = new Scanner(System.in);
-        String choice = "";
         String finalSelection = "";
         try {
             BufferedReader bfr = new BufferedReader(new FileReader("accountDetails.txt"));
             String line = "";
+            int choice = 0;
             ArrayList<String> foundPeople = new ArrayList<>();
             boolean found = false;
             int finalNumber = 0;
+            String invalidInput = "Invalid input. Please try again.";
             System.out.println("Select one of the options below:\n1. View list of tutors\n2. Search for a tutor");
-            choice = scanner.nextLine();
-            while (!choice.equals("1") && !choice.equals("2")) {
-                System.out.println("Invalid option! Please try again.");
-                System.out.println("Select one of the options below:\n1. View list of tutor\n2. Search for a tutor");
-                choice = scanner.nextLine();
+            while (true) {
+                try {
+                    choice = scanner.nextInt();
+                    scanner.nextLine();
+                    if (choice == 1 || choice == 2)
+                        break;
+                    System.out.println(invalidInput);
+                } catch (InputMismatchException e) {
+                    scanner.next();
+                    System.out.println(invalidInput);
+                }
             }
             int counter = 1;
-            if (choice.equals("1")) {
+            if (choice == 1) {
                 line = bfr.readLine();
                 while (line != null) {
                     String[] splitLine = line.split(":");
@@ -116,7 +155,18 @@ public class View {
                     line = bfr.readLine();
                 }
                 System.out.println("Select from tutors found:");
-                finalNumber = scanner.nextInt();
+                while (true) {
+                    try {
+                        finalNumber = scanner.nextInt();
+                        scanner.nextLine();
+                        if (finalNumber < counter && finalNumber > 0)
+                            break;
+                        System.out.println(invalidInput);
+                    } catch (InputMismatchException e) {
+                        System.out.println(invalidInput);
+                        scanner.next();
+                    }
+                }
                 finalSelection = foundPeople.get(finalNumber - 1);
             } else {
                 String searchAgain = "";
@@ -140,29 +190,41 @@ public class View {
                     bfr.close();
                     if (!found) {
                         System.out.println("User not found! Would you like to search again? (Yes/No)");
-                        searchAgain = scanner.nextLine();
-                        if (searchAgain.equalsIgnoreCase("no")) {
-                            System.out.println("Thank you for using TutorFinder! Goodbye.");
-                            return null;
+                        while (true) {//!searchAgain.equalsIgnoreCase("no") && !searchAgain.equalsIgnoreCase("yes")) {
+                            searchAgain = scanner.nextLine();
+                            if (searchAgain.equalsIgnoreCase("no")) {
+                                System.out.println("Thank you for using TutorFinder! Goodbye.");
+                                return null;
+                            } else if (searchAgain.equalsIgnoreCase("yes")) {
+                                break;
+                            }
+                            System.out.println(invalidInput);
                         }
                         bfr = new BufferedReader(new FileReader("accountDetails.txt"));
                     }
                 } while (!found && searchAgain.equalsIgnoreCase("yes"));
-
                 if (found) {
                     System.out.println("Select from tutors found:");
-                    finalNumber = scanner.nextInt();
+                    while (true) {
+                        try {
+                            finalNumber = scanner.nextInt();
+                            scanner.nextLine();
+                            if (finalNumber < counter && finalNumber > 0)
+                                break;
+                            System.out.println(invalidInput);
+                        } catch (InputMismatchException e) {
+                            System.out.println(invalidInput);
+                            scanner.next();
+                        }
+                    }
                     finalSelection = foundPeople.get(finalNumber - 1);
                 }
-                if (searchAgain.equalsIgnoreCase("no")) {
-                    System.out.println("Thank you for using TutorFinder! Goodbye.");
-                    return null;
-                }
             }
-        } catch (IOException e) {
+        } catch(IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
         return new User(finalSelection, false);
     }
+
 }
