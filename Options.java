b7ec08af83d094e347099604259c427f4f961bed
tandon35 <tandon35@purdue.edu>
2023-11-11 22:03:32 -0500

    Added send message functionality, commented which functions not to touch yet

--- Options.java
+++ Options.java
@@ -5,15 +5,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class Options {
 
     private User userTerminal;
     private User userSelected;
+    private String senderConvoFileName;
+    private String receiverConvoFileName;
 
     public Options(User userTerminal, User userSelected) {
         this.userTerminal = userTerminal;
         this.userSelected = userSelected;
+        this.senderConvoFileName = userTerminal.getUsername() + "_" + userSelected.getUsername() + ".txt";
+        this.receiverConvoFileName = userSelected.getUsername() + "_" + userTerminal.getUsername() + ".txt";
     }
 
     public void viewMenu() {
@@ -36,16 +42,66 @@ public class Options {
                     do {
                         System.out.println("""
                                 (1) Send Message
-                                (2) Go Back""");
+                                (2) Edit message
+                                (3) Delete message
+                                (4) Go Back""");
                         String convoOption = scanner.nextLine();
                         switch (convoOption) {
                             case "1":
                                 // Send message
                                 System.out.println("Enter your message:");
                                 String message = scanner.nextLine();
-                                
+                                sendMessage(message);
                                 break;
                             case "2":
+                                //Edit message
+                                System.out.println("Which message do you want to edit?");
+                                int bound = displayMessages();
+                                String messageEdit = "";
+                                boolean valid = true;
+                                do {
+                                    try {
+                                        int messageIndex = Integer.parseInt(scanner.nextLine());
+                                        if (messageIndex < 1 || messageIndex > bound) {
+                                            System.out.println("Please enter a a number between 1 and " + bound + "!");
+                                            valid = false;
+                                        } else {
+                                            valid = true;
+                                        }
+                                        messageEdit = findMessage(messageIndex);
+                                    } catch (NumberFormatException e) {
+                                        System.out.println("Please enter a valid input!");
+                                    }
+
+                                    System.out.println("Enter your new message:");
+                                    String newMessage = scanner.nextLine();
+                                    editMessage(messageEdit, newMessage);
+                                } while (!valid);
+                                break;
+                            case "3":
+                                // Delete message
+                                System.out.println("Which message do you want to delete?");
+                                int bound2 = displayMessages();
+                                String messageDelete = "";
+                                boolean valid2 = true;
+                                do {
+                                    try {
+                                        int messageIndex = Integer.parseInt(scanner.nextLine());
+                                        if (messageIndex < 1 || messageIndex > bound2) {
+                                            System.out.println("Please enter a a number between 1 and " + bound2 + "!");
+                                            valid2 = false;
+                                        } else {
+                                            valid2 = true;
+                                        }
+                                        messageDelete = findMessage(messageIndex);
+                                    } catch (NumberFormatException e) {
+                                        System.out.println("Please enter a valid input!");
+                                    }
+
+                                    deleteMessage(messageDelete);
+                                } while (!valid2);
+                                break;
+                            case "4":
                                 // Go back to viewMenu();
                                 inConvo = false;
                                 viewMenu();
@@ -56,7 +112,7 @@ public class Options {
                         }
 
                     } while (inConvo);
-                    
+
                     break;
                 case "2":
                     // Export conversation to a CSV file.
@@ -92,14 +148,13 @@ public class Options {
     }
 
     public String getConversation() {
-        String convoFileName = userTerminal.getUsername() + "_" + userSelected.getUsername() + ".txt";
         String convo = "";
         try {
-            File f = new File(convoFileName);
-            if(!f.exists()) {
+            File f = new File(senderConvoFileName);
+            if (!f.exists()) {
                 f.createNewFile();
             }
-            BufferedReader bfr = new BufferedReader(new FileReader(convoFileName));
+            BufferedReader bfr = new BufferedReader(new FileReader(senderConvoFileName));
             String line;
             while ((line = bfr.readLine()) != null) {
                 convo += line + "\n";
@@ -109,6 +164,88 @@ public class Options {
             return null;
         }
         return convo;
+    }
+
+    public void sendMessage(String message) {
+        try {
+            PrintWriter pw = new PrintWriter(new FileWriter(senderConvoFileName, true));
+            LocalDateTime timestamp = LocalDateTime.now();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedTimestamp = timestamp.format(formatter);
+            pw.println(userTerminal.getUsername() + "(" + formattedTimestamp + ")" + ":" + message);
+            pw.flush();
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("Error writing to file.");
+        }
+    }
+
+    //Don't touch, not tested yet
+    public void editMessage(String message, String newMessage) {
+        try (BufferedReader bfr = new BufferedReader(new FileReader(senderConvoFileName))) {
+            PrintWriter pw = new PrintWriter(new FileWriter(senderConvoFileName, true));
+            String line = bfr.readLine();
+            while (line != null) {
+                if (line.substring(line.indexOf(":")).equals(message)) {
+                    LocalDateTime timestamp = LocalDateTime.now();
+                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                    String formattedTimestamp = timestamp.format(formatter);
+                    pw.println(userTerminal.getUsername() + "(" + formattedTimestamp + ")" + ":" + newMessage);
+                    pw.flush();
+                }
+            }
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+    }
+
+    //Don't touch, not tested yet
+    public void deleteMessage(String message) {
+        try (BufferedReader bfr = new BufferedReader(new FileReader(senderConvoFileName))) {
+            PrintWriter pw = new PrintWriter(new FileWriter(senderConvoFileName, false));
+            String line = bfr.readLine();
+            while (line != null) {
+                if (!line.substring(line.indexOf(":")).equals(message)) {
+                    pw.println(line);
+                    pw.flush();
+                }
+            }
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+    }
+
+    public int displayMessages() {
+        try (BufferedReader bfr = new BufferedReader(new FileReader(senderConvoFileName))) {
+            String line;
+            int i = 1;
+            while ((line = bfr.readLine()) != null) {
+                System.out.println("(" + i + ") " + line);
+                i++;
+            }
+            return i;
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+        return 0;
+    }
+
+    public String findMessage(int index) {
+        try (BufferedReader bfr = new BufferedReader(new FileReader(senderConvoFileName))) {
+            String line;
+            int i = 1;
+            while ((line = bfr.readLine()) != null) {
+                if (i == index) {
+                    return line;
+                }
+                i++;
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading file.");
+        }
+        return null;
     }
 
 }
