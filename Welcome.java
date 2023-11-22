54cfb873554d69bc9dcf1b56d085c0317324521f
SamBalakhanei <thesamloop@gmail.com>
2023-11-21 21:53:42 -0500

    formatting and removing old code

--- Welcome.java
+++ Welcome.java
@@ -8,9 +8,11 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 
 import javax.swing.*;
+
 /**
  *
- * This class begins the communication with the user and takes care of the login and signup page.
+ * This class begins the communication with the user and takes care of the login
+ * and signup page.
  *
  *
  * @author Niharika Raj, Saahil Mathur, Sam Balakhanei, Abhi Tandon
@@ -20,23 +22,13 @@ public class Welcome extends JComponent implements Runnable {
     Image image;
     Container content;
 
-
-
     JTextField username; // = String username
     JPasswordField password; // = String password
-    JCheckBox student; //checked = student, unchecked = tutor
-    JButton loginButtonWhich; //choose if they want to login
-    JButton signUpButtonWhich;  //choose if they want to sign up
-    JButton loginButton; //choose to actually submit login information
-    JButton signUpButton; //choose to actually submit sign up information
-
-
-
-
-
-
-
-
+    JCheckBox student; // checked = student, unchecked = tutor
+    JButton loginButtonWhich; // choose if they want to login
+    JButton signUpButtonWhich; // choose if they want to sign up
+    JButton loginButton; // choose to actually submit login information
+    JButton signUpButton; // choose to actually submit sign up information
 
     public void run() {
         JFrame frame = new JFrame("Tutor Messenger");
@@ -59,8 +51,7 @@ public class Welcome extends JComponent implements Runnable {
         panel.add(loginButtonWhich);
         panel.add(signUpButtonWhich);
         content.add(panel, BorderLayout.CENTER);
-        frame.validate(); //refreshes the screen
-        
+        frame.validate(); // refreshes the screen
 
     }
 
@@ -75,38 +66,37 @@ public class Welcome extends JComponent implements Runnable {
                 String passwordString = new String(password.getPassword());
                 User user = new User(username.getText(), passwordString);
                 if (!userExists(user)) {
-                    JOptionPane.showMessageDialog(null, 
-                    "User does not exist!", "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null,
+                            "User does not exist!", "Error", JOptionPane.ERROR_MESSAGE);
                 } else if (!validateUser(user)) {
-                    JOptionPane.showMessageDialog(null, 
-                    "Incorrect Password!", "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null,
+                            "Incorrect Password!", "Error", JOptionPane.ERROR_MESSAGE);
                 } else {
-                    JOptionPane.showMessageDialog(null, 
-                    "Welcome " + user.getUsername() + "!", "Success!", JOptionPane.PLAIN_MESSAGE);
-                    //Go to next screen with:
+                    JOptionPane.showMessageDialog(null,
+                            "Welcome " + user.getUsername() + "!", "Success!", JOptionPane.PLAIN_MESSAGE);
+                    // Go to next screen with:
                     // if (user.getUserType()) {
-                    //     View.findTutor(user.getUsername(), user);
+                    // View.findTutor(user.getUsername(), user);
                     // } else {
-                    //     View.findStudent(user.getUsername(), user);
+                    // View.findStudent(user.getUsername(), user);
                     // }
                 }
 
             } else if (e.getSource() == signUpButton) {
                 String passwordString = new String(password.getPassword());
-                User user = new User(username.getText(), passwordString, student.isSelected()); //have to check if student box is checked
+                User user = new User(username.getText(), passwordString, student.isSelected()); // have to check if
+                                                                                                // student box is
+                                                                                                // checked
                 if (createUser(user)) {
-                    JOptionPane.showMessageDialog(null, 
-                    "Account created successfully!", "Success!", JOptionPane.PLAIN_MESSAGE);
+                    JOptionPane.showMessageDialog(null,
+                            "Account created successfully!", "Success!", JOptionPane.PLAIN_MESSAGE);
                 }
 
-
-
-
-                //Go to next screen with:
+                // Go to next screen with:
                 // if (user.getUserType()) {
-                //     View.findTutor(user.getUsername(), user);
+                // View.findTutor(user.getUsername(), user);
                 // } else {
-                //     View.findStudent(user.getUsername(), user);
+                // View.findStudent(user.getUsername(), user);
                 // }
             }
         }
@@ -157,83 +147,14 @@ public class Welcome extends JComponent implements Runnable {
         content.repaint();
 
     }
+
     public static void main(String[] args) {
         Welcome welcome = new Welcome();
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 welcome.run();
             }
-    });
-
-        // Scanner scan = new Scanner(System.in);
-        // String username;
-        // String password;
-        // User user;
-        // String choice;
-        // boolean type; // true = student, false = tutor
-        // do {
-        //     System.out.print("Welcome to the tutor marketplace!\n(1) Log In\n(2) Sign Up\n(3) Exit\n");
-        //     choice = scan.nextLine();
-        //     switch (choice) {
-        //         case "1":
-        //             System.out.print("Enter your username: ");
-        //             username = scan.nextLine();
-        //             System.out.print("Enter your password: ");
-        //             password = scan.nextLine();
-        //             user = new User(username, password);
-        //             if (!userExists(user)) {
-        //                 System.out.println("User does not exist!");
-        //                 choice = "-1";
-        //                 user = null;
-        //             } else if (!validateUser(user)) {
-        //                 System.out.println("Incorrect password!");
-        //                 choice = "-1";
-        //                 user = null;
-        //             } else {
-        //                 System.out.println("Welcome " + username + "!");
-        //             }
-        //             break;
-        //         case "2":
-        //             System.out.print("Enter your username: ");
-        //             username = scan.nextLine();
-        //             System.out.print("Enter your password: ");
-        //             password = scan.nextLine();
-        //             boolean valid = false;
-        //             boolean temp = false;
-        //             do {
-        //                 System.out.print("Are you a student or a tutor?\n (1) Student\n (2) Tutor\n");
-        //                 String option = scan.nextLine();
-        //                 if (option.equals("1") || option.equals("2")) {
-        //                     temp = option.equals("1");
-        //                     valid = true;
-        //                 } else {
-        //                     System.out.println("Please enter a valid input!");
-        //                 }
-        //             } while (!valid);
-        //             type = temp;
-        //             user = new User(username, password, type);
-        //             if (!createUser(user)) {
-        //                 choice = "-1";
-        //                 user = null;
-        //             }
-        //             break;
-        //         case "3":
-        //             System.out.println("Goodbye!");
-        //             return;
-        //         default:
-        //             System.out.println("Invalid Option!");
-        //             user = null;
-        //             break;
-        //     }
-        // } while (user == null);
-
-        // if (user.getUserType()) {
-        //     View.findTutor(user.getUsername(), user);
-        // } else {
-        //     View.findStudent(user.getUsername(), user);
-        // }
-
-        // scan.close();
+        });
     }
 
     // Checks if user exists in accountDetails.txt (not case sensitive)
@@ -294,14 +215,14 @@ public class Welcome extends JComponent implements Runnable {
                 String username = details[0];
                 if (user.getUsername().equals(username)) {
                     JOptionPane.showMessageDialog(null, "User already exists!",
-                     "Error", JOptionPane.ERROR_MESSAGE);
+                            "Error", JOptionPane.ERROR_MESSAGE);
 
                     return false;
                 }
                 if (user.getUsername().contains(":") || user.getPassword().contains(":")) {
-                    JOptionPane.showMessageDialog(null, 
-                    "Username and/or password cannot contain ':'",
-                     "Error", JOptionPane.ERROR_MESSAGE);
+                    JOptionPane.showMessageDialog(null,
+                            "Username and/or password cannot contain ':'",
+                            "Error", JOptionPane.ERROR_MESSAGE);
                     return false;
                 }
             }
